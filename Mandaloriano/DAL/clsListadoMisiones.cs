1a0903bc07d7fd34ac3e2f15bea144605aa9ac8c
siu <francisco.rodriguez@iesnervion.es>
2023-11-16 11:13:42 +0100

    Agregar archivos de proyecto.

---
+++ Mandaloriano/DAL/clsListadoMisiones.cs
@@ -0,0 +1,13 @@
+﻿using Mandaloriano.Models.Entidades;
+
+namespace Mandaloriano.DAL
+{
+    public static class clsListadoMisiones
+    {
+        public static List<clsMision> listaMisiones()
+        {
+            List<clsMision> lista = new List<clsMision>(){
+            return lista;
+        }
+    }
+}
---
+++ Mandaloriano/Models/Entidades/clsMision.cs
@@ -0,0 +1,46 @@
+﻿namespace Mandaloriano.Models.Entidades
+{
+    public class clsMision
+    {
+        #region atributos
+        private int idMision;
+        private string objetivoMision;
+        private string planetaMision;
+        private string recompensaMision;
+        #endregion
+
+        #region constructores
+        public clsMision() { }
+        public clsMision(int idMision, string objetivoMision, string planetaMision, string recompensaMision)
+        {
+            this.idMision = idMision;
+            this.objetivoMision = objetivoMision;
+            this.planetaMision = planetaMision;
+            this.recompensaMision = recompensaMision;
+        }
+        #endregion
+
+        #region propiedades
+        public int IdMision
+        {
+            get { return idMision; }
+            set { idMision = value; }
+        }
+        public string ObjetivoMision
+        {
+            get { return objetivoMision; }
+            set { objetivoMision = value; }
+        }
+        public string PlanetaMision
+        {
+            get { return planetaMision; }
+            set { planetaMision = value; }
+        }
+        public string RecompensaMision
+        {
+            get { return recompensaMision; }
+            set { recompensaMision = value;}
+        }
+        #endregion
+    }
+}
